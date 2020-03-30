d4f86d7f2d59ae3bd739eaa0d753d87337e03a5e
alvrubtap-sys <alvrubtap@alum.us.es>
2020-03-30 18:26:00 +0200

    Álvaro's Upates

--- src/main/java/org/springframework/samples/petclinic/repository/InterventionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/InterventionRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.samples.petclinic.model.Intervention;
+import org.springframework.samples.petclinic.model.Vet;
 
 public interface InterventionRepository {
 
@@ -16,4 +18,7 @@ public interface InterventionRepository {
 	List<Intervention> findInterventionByPetId(Integer petId);
 
 	Optional<Intervention> findById(int id) throws DataAccessException;
+
+	@Query("SELECT vet FROM Vet vet WHERE vet.user.username=:username")
+	Optional<Vet> findByUsername(String username);
 }
