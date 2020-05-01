fea8d780be9a689b1cc73df87b5ef3cf5a822011
Manuel Cañizares Juan <mancanjua@alum.us.es>
2020-05-01 05:06:27 +0200

    UI Testing and Advanced UI Testing finished

--- src/test/java/org/springframework/samples/petclinic/ui/VetAddsPrescriptionPositiveUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/VetAddsPrescriptionPositiveUITest.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
@@ -15,84 +16,98 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VetAddsPrescriptionPositiveUITest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	
+	@LocalServerPort
+	private int port;
 
-  @BeforeEach
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Test
-  public void testUS007Positive() throws Exception {
-    driver.get("http://localhost:8080/");
-    driver.findElement(By.linkText("Login")).click();
-    driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("vet1");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("v3t1");
-    driver.findElement(By.linkText("Find owners")).click();
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.linkText("Jean Coleman")).click();
-    driver.findElement(By.xpath("(//a[contains(text(),'Medical History')])[2]")).click();
-    driver.findElement(By.linkText("2013-01-01")).click();
-    driver.findElement(By.linkText("Add prescription")).click();
-    new Select(driver.findElement(By.id("medicine"))).selectByVisibleText("Cat medicine");
-    driver.findElement(By.xpath("//option[@value='Cat medicine']")).click();
-    driver.findElement(By.id("dose")).click();
-    driver.findElement(By.id("dose")).clear();
-    driver.findElement(By.id("dose")).sendKeys("test dose");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    assertEquals("test dose", driver.findElement(By.xpath("//table[@id='prescriptionsTable']/tbody/tr[2]/td[2]")).getText());
-    assertEquals("Cat medicine", driver.findElement(By.xpath("(//a[contains(text(),'Cat medicine')])[2]")).getText());
-  }
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToGeckoDriver = "./src/test/resources/geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
+		driver = new FirefoxDriver();
+		baseUrl = "https://www.google.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @AfterEach
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@Test
+	public void testUS007Positive() throws Exception {
+		driver.get("http://localhost:" + port);
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement(By.id("username")).click();
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("vet1");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("v3t1");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Find owners')]")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.linkText("Jean Coleman")).click();
+		driver.findElement(By.xpath("(//a[contains(text(),'Medical History')])[2]")).click();
+		driver.findElement(By.linkText("2013-01-01")).click();
+		driver.findElement(By.linkText("Add prescription")).click();
+		new Select(driver.findElement(By.id("medicine"))).selectByVisibleText("Cat medicine");
+		driver.findElement(By.xpath("//option[@value='Cat medicine']")).click();
+		driver.findElement(By.id("dose")).click();
+		driver.findElement(By.id("dose")).clear();
+		driver.findElement(By.id("dose")).sendKeys("test dose");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		assertEquals("test dose",
+				driver.findElement(By.xpath("//table[@id='prescriptionsTable']/tbody/tr[2]/td[2]")).getText());
+		assertEquals("Cat medicine",
+				driver.findElement(By.xpath("(//a[contains(text(),'Cat medicine')])[2]")).getText());
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	@AfterEach
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
--- src/test/java/org/springframework/samples/petclinic/ui/VetAdoptsPetNegativeUITest.java
+++ src/test/java/org/springframework/samples/petclinic/ui/VetAdoptsPetNegativeUITest.java
@@ -8,82 +8,95 @@ import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class VetAdoptsPetNegativeUITest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
 
-  @BeforeEach
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@LocalServerPort
+	private int port;
 
-  @Test
-  public void testUS012Negative() throws Exception {
-    driver.get("http://localhost:8080/");
-    driver.findElement(By.linkText("Login")).click();
-    driver.findElement(By.id("username")).click();
-    driver.findElement(By.id("username")).clear();
-    driver.findElement(By.id("username")).sendKeys("vet1");
-    driver.findElement(By.id("password")).clear();
-    driver.findElement(By.id("password")).sendKeys("v3t1");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.linkText("vet1")).click();
-    driver.findElement(By.linkText("Manage homeless pets")).click();
-    driver.findElement(By.linkText("Adopt")).click();
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    assertEquals("no puede ser null", driver.findElement(By.xpath("//form[@id='adoption']/div/div/div/div/span[2]")).getText());
-  }
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @AfterEach
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@BeforeEach
+	public void setUp() throws Exception {
+		String pathToGeckoDriver = "./src/test/resources/geckodriver.exe";
+		System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
+		driver = new FirefoxDriver();
+		baseUrl = "https://www.google.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	@Test
+	public void testUS012Negative() throws Exception {
+		driver.get("http://localhost:" + port);
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		driver.findElement(By.id("username")).click();
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("vet1");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("v3t1");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//strong[contains(text(),'vet1')]")).click();
+		driver.findElement(By.linkText("Manage homeless pets")).click();
+		driver.findElement(By.linkText("Adopt")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		assertEquals("no puede ser null",
+				driver.findElement(By.xpath("//form[@id='adoption']/div/div/div/div/span[2]")).getText());
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	@AfterEach
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
+
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
+
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
