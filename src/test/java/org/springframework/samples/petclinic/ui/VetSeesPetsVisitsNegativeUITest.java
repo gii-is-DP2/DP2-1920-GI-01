806728b3ab5eddb9a61dcbbd0b53c9fb0f71222d
diana <diana.buksa1@gmail.com>
2020-05-02 17:24:24 +0200

    asserts finished

---
+++ src/test/java/org/springframework/samples/petclinic/ui/VetSeesPetsVisitsNegativeUITest.java
@@ -0,0 +1,101 @@
+package org.springframework.samples.petclinic.ui;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class VetSeesPetsVisitsNegativeUITest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @LocalServerPort
+  private int port;
+  
+  @BeforeEach
+  public void setUp() throws Exception {
+	String pathToGeckoDriver = "./src/test/resources/geckodriver.exe";
+	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+   
+  public void testTrainerAddsRehab () throws Exception {
+	  
+	  driver.manage().window().maximize();
+	  VetTryingToSeePetsVisitsWithoutLogingIn(driver, port);
+	  
+  }
+  
+  
+  
+  
+  public void VetTryingToSeePetsVisitsWithoutLogingIn(WebDriver driver, int port) throws Exception {
+	  driver.get("http://localhost:" + port);
+	    new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Login')]")));
+	    driver.findElement(By.xpath("//div[@id='main-navbar']/ul/li[2]/a/span[2]")).click();
+	    driver.findElement(By.xpath("//h2")).click();
+	    assertEquals("Please sign in", driver.findElement(By.xpath("//h2")).getText());
+
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
