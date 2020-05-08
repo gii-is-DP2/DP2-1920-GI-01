329fe059054706ff2c522334e5cbd37013eaa4b2
Álvaro Rubia Tapia <56016868+alvrubtap-sys@users.noreply.github.com>
2020-05-09 01:21:34 +0200

    Update InterventionCReationStepDefinitions.java

--- src/test/java/org/springframework/samples/petclinic/bdd/stepdefinitions/InterventionCReationStepDefinitions.java
+++ src/test/java/org/springframework/samples/petclinic/bdd/stepdefinitions/InterventionCReationStepDefinitions.java
@@ -36,9 +36,9 @@ public class InterventionCReationStepDefinitions extends AbstractStep {
 
 	@Then("the new intervention appears in the owners details page")
 	public void the_new_intervention_appears_in_the_owners_details_page() {
-		Assert.assertEquals("Descripcion", this.getDriver().findElement(By.xpath("//b")).getText());
-		Assert.assertEquals("2020/10/01", this.getDriver().findElement(By.xpath("//tr[2]/td")).getText());
-		Assert.assertEquals("1", this.getDriver().findElement(By.xpath("//tr[3]/td/b")).getText());
+		Assert.assertEquals("Descripcion", this.getDriver().findElement(By.xpath("//table[3]/tbody/tr/td[2]/table/tbody/tr/td[2]")).getText());
+		Assert.assertEquals("2020-10-01", this.getDriver().findElement(By.xpath("//table[3]/tbody/tr/td[2]/table/tbody/tr/td")).getText());
+		Assert.assertEquals("1", this.getDriver().findElement(By.xpath("//td[3]")).getText());
 		this.stopDriver();
 	}
 
