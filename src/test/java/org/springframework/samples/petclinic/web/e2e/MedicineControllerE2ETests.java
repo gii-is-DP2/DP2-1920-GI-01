c37b1bd5c56107350a7ee24c6b0aaf60216f9240
Manuel Cañizares Juan <mancanjua@alum.us.es>
2020-05-09 02:54:55 +0200

    E2E Tests fixed

--- src/test/java/org/springframework/samples/petclinic/web/e2e/MedicineControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/MedicineControllerE2ETests.java
@@ -2,7 +2,6 @@ package org.springframework.samples.petclinic.web.e2e;
 
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -10,18 +9,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.samples.petclinic.model.Medicine;
-import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.service.MedicineService;
-import org.springframework.samples.petclinic.service.PetService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -34,24 +26,8 @@ import org.springframework.transaction.annotation.Transactional;
 /*@TestPropertySource(locations = "classpath:application-mysql.properties")*/
 public class MedicineControllerE2ETests {
 	
-	@MockBean
-	private MedicineService medicineService;
-	
-	@MockBean
-	private PetService petService;
-	
 	@Autowired
 	private MockMvc mockMvc;
-	
-	@BeforeEach
-	void setup() {
-		PetType petType = new PetType();
-		petType.setId(2);
-		petType.setName("dog");
-		given(this.petService.findPetTypes()).willReturn(Lists.newArrayList(petType));
-		given(this.medicineService.findMedicineById(1)).willReturn(new Medicine());
-		given(this.medicineService.findMedicineById(0)).willReturn(null);
-	}
 	
 	@WithMockUser(username = "vet1", authorities = {"veterinarian"})
 	@Test
@@ -73,7 +49,7 @@ public class MedicineControllerE2ETests {
 							.param("maker", "Test")
 							.param("petType", "dog"))
 			   .andExpect(status().is3xxRedirection())
-			   .andExpect(view().name("redirect:/medicine/show?id=null"));
+			   .andExpect(view().name("redirect:/medicine/show?id=3"));
 	}
 	
 	@WithMockUser(username = "vet1", authorities = {"veterinarian"})
@@ -197,29 +173,7 @@ public class MedicineControllerE2ETests {
 	
 	@WithMockUser(username = "vet1", authorities = {"veterinarian"})
 	@Test
-	void testListMedicineOneResult() throws Exception {
-		given(this.medicineService.findManyMedicineByName("")).willReturn(Lists.newArrayList(new Medicine()));
-		
-		mockMvc.perform(get("/medicine/list"))
-			   .andExpect(status().is3xxRedirection())
-			   .andExpect(view().name("redirect:/medicine/show?id=null"));
-	}
-	
-	@WithMockUser(username = "vet1", authorities = {"veterinarian"})
-	@Test
-	void testListMedicineNoResults() throws Exception {
-		given(this.medicineService.findManyMedicineByName("")).willReturn(Lists.newArrayList());
-		
-		mockMvc.perform(get("/medicine/list"))
-			   .andExpect(status().isOk())
-			   .andExpect(view().name("medicine/list"));
-	}
-	
-	@WithMockUser(username = "vet1", authorities = {"veterinarian"})
-	@Test
 	void testListMedicineMultipleResults() throws Exception {
-		given(this.medicineService.findManyMedicineByName("")).willReturn(Lists.newArrayList(new Medicine(), new Medicine()));
-		
 		mockMvc.perform(get("/medicine/list"))
 			   .andExpect(status().isOk())
 			   .andExpect(view().name("medicine/list"))
