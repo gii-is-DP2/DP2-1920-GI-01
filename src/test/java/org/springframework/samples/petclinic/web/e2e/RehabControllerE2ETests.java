8ed762873918fb41754e44897ae4a52067372916
diana <diana.buksa1@gmail.com>
2020-05-09 08:53:36 +0200

    RehabControllerE2ETests updated

--- src/test/java/org/springframework/samples/petclinic/web/e2e/RehabControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/web/e2e/RehabControllerE2ETests.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.web.e2e;
 
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -9,25 +11,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.Rehab;
+import org.springframework.samples.petclinic.model.Trainer;
+import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.service.PetService;
 import org.springframework.samples.petclinic.service.RehabService;
+import org.springframework.samples.petclinic.service.TrainerService;
+import org.springframework.samples.petclinic.web.RehabController;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @ExtendWith(SpringExtension.class)
@@ -35,22 +48,33 @@ import org.springframework.transaction.annotation.Transactional;
 		webEnvironment=SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Transactional
-public class RehabControllerE2ETests {
+class RehabControllerE2ETests {
 
 	private static final int TEST_PET_ID = 1;
+	
+	@Autowired
+	private RehabController rehabController;
+	
 
 	@MockBean
 	private RehabService	rehabService;
 	
 	@MockBean
 	private PetService		petService;
+
+	@MockBean
+	private TrainerService 	trainerService;
 	
+		
 	@Autowired
 	private MockMvc mockMvc;
 	
 	
 	@BeforeEach
 	void setup() {
+		
+
+		
 		Rehab rehab = new Rehab();
 		rehab.setDate(LocalDate.now());
 		rehab.setTime(1);
@@ -58,56 +82,59 @@ public class RehabControllerE2ETests {
 		
 		Pet pet = new Pet();
 		pet.setBirthDate(LocalDate.of(2019, 02, 14));
-		pet.setName("Maya");
+		pet.setName("Reksis");
 
-		BDDMockito.given(this.petService.findPetById(RehabControllerE2ETests.TEST_PET_ID)).willReturn(pet);
-
+		given(this.petService.findPetById(RehabControllerE2ETests.TEST_PET_ID)).willReturn(pet);
 	}
 	
-	/* testing opening the rehab form*/
-	 @WithMockUser(username = "trainer1", authorities = {"trainer"})
-	    @Test
-		void OpeningANewRehabForm() throws Exception {
-			mockMvc.perform(get("/owners/*/pets/{petId}/rehab/new", TEST_PET_ID)).andExpect(status().isOk())
-					.andExpect(view().name("pets/createOrUpdateRehabForm"));
-		}
-	 
-			 /* Testing when some of the input parameters are not filled */
-				@WithMockUser(username = "trainer1", authorities = {"trainer"})
-				@Test
-				void UnsuccessfulRehabFormAddingWithError() throws Exception {
-					this.mockMvc.perform(MockMvcRequestBuilders.post("/owners/*/pets/" + RehabControllerE2ETests.TEST_PET_ID + "/rehab/new")//
-						.with(SecurityMockMvcRequestPostProcessors.csrf())//
-						.param("date", "")//
-						.param("time", "")//
-						.param("description", "description"))//
-						.andExpect(MockMvcResultMatchers.model().attributeHasFieldErrors("rehab"))//
-						.andExpect(MockMvcResultMatchers.status().isOk())//
-						.andExpect(MockMvcResultMatchers.view().name("pets/createOrUpdateRehabForm"));//
-				}
-				
-				/* successfully adding a new rehab*/
-				@WithMockUser(username = "trainer1", authorities = {"trainer"})
-			    @Test
-				void AddingANewRehab() throws Exception {
-					mockMvc.perform(post("/owners/*/pets/{petId}/rehab/new", TEST_PET_ID)
-							.with(csrf())
-							.param("date", "2021/04/04")
-							.param("time", "2")
-							.param("description", "Description"))                                
-			                .andExpect(status().is3xxRedirection())
-						.andExpect(view().name("redirect:/owners/{ownerId}"));
-			 }
+
+	/* Opening new rehab form*/
+	
+	@WithMockUser(username = "trainer1", authorities = {"trainer"})
+	@Test
+	void testAddingNewRehabForm() throws Exception {
+		mockMvc.perform(get("/owners/*/pets/" + RehabControllerE2ETests.TEST_PET_ID + "/rehab/new"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("pets/createOrUpdateRehabForm"))
+				.andExpect(model().attributeExists("rehab"));
+	}
+	
+	/* Creating a new rehab, successful */
+	
+	@WithMockUser(username = "trainer1", authorities = {"trainer"})
+	@Test
+	void testProcessCreateRehabFormIsSuccessful() throws Exception {
+		this.mockMvc.perform(post("/owners/*/pets/" + RehabControllerE2ETests.TEST_PET_ID +"/rehab/new")//
+			.with(csrf())//
+			.param("date", "2020/05/09")//
+			.param("time", "3")//
+			.param("description", "Testing1"))//
+			.andExpect(status().is3xxRedirection())//
+			.andExpect(view().name("redirect:/owners/{ownerId}"));
+	}
+
+	/* Creating a new rehab, unsuccessful */
+	
+	@WithMockUser(username = "trainer1", authorities = {"trainer"})
+	@Test
+	void testProcessCreateRehabFormHasErrors() throws Exception {
+		this.mockMvc.perform(post("/owners/*/pets/" + RehabControllerE2ETests.TEST_PET_ID + "/rehab/new")//
+			.with(csrf())//
+			.param("date", "2020/05/09")//
+			.param("time", "")//
+			.param("description", "Testing"))//
+			.andExpect(model().attributeHasFieldErrors("rehab"))//
+			.andExpect(status().isOk())//
+			.andExpect(view().name("pets/createOrUpdateRehabForm"));//
+	}
+
+
 		
-				/* seeing pets rehabs */
-				
-				@WithMockUser(username = "trainer1", authorities = {"trainer"})
-			    @Test
-				void AddedRehabListCheck() throws Exception {
-					mockMvc.perform(get("/owners/*/pets/{petId}/rehab", TEST_PET_ID)).andExpect(status().isOk())
-							.andExpect(model().attributeExists("rehab")).andExpect(view().name("rehabList"));
-				}
-			
+		
 }
+
+
+				 
+				 
 				
 
