5f8d7268456e12d1cf16e8b8783eaed7b3730dd1
Álvaro Rubia Tapia <56016868+alvrubtap-sys@users.noreply.github.com>
2020-05-19 10:09:29 +0200

    Create us17- OwnerSeesVetPersonalInformation.scala

---
+++ src/performance/us17- OwnerSeesVetPersonalInformation.scala
@@ -0,0 +1,69 @@
+  
+package dp2
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class us17 extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://www.dp2.com")
+		.inferHtmlResources(BlackList(""".*.jar""", """.*.css""", """.*.js""", """.*.png""", """.*.ico"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-ES,es;q=0.9,en;q=0.8")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36")
+
+	val headers_0 = Map(
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_2 = Map(
+		"Accept" -> "image/webp,image/apng,image/*,*/*;q=0.8",
+		"Proxy-Connection" -> "keep-alive")
+
+	val headers_3 = Map(
+		"Origin" -> "http://www.dp2.com",
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+
+
+	val scn = scenario("us17")
+		.exec(http("home")
+			.get("/")
+			.headers(headers_0))
+		.pause(40)
+		// home
+		.exec(http("login")
+			.get("/login")
+			.headers(headers_0)
+			.resources(http("request_2")
+			.get("/login")
+			.headers(headers_2)))
+		.pause(6)
+		// login
+		.exec(http("logged")
+			.post("/login")
+			.headers(headers_3)
+			.formParam("username", "owner1")
+			.formParam("password", "0wn3r")
+			.formParam("_csrf", "5cc54564-e50a-41a6-96de-7538e64ade6b"))
+		.pause(12)
+		// logged
+		.exec(http("veterinarianListing")
+			.get("/vets")
+			.headers(headers_0))
+		.pause(59)
+		// veterinarianListing
+		.exec(http("vetShow")
+			.get("/vets/1")
+			.headers(headers_0))
+		.pause(5)
+		// vetShow
+
+	setUp(scn.inject(atOnceUsers(2000))).protocols(httpProtocol)
+}
