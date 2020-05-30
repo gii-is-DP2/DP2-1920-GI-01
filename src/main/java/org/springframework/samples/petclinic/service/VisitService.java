740b4a7d30a871fc3865171d3d338e52e683553b
alvrubtap-sys <alvrubtap@alum.us.es>
2020-05-30 20:03:33 +0200

    Refactoring Alvaro

--- src/main/java/org/springframework/samples/petclinic/service/VisitService.java
+++ src/main/java/org/springframework/samples/petclinic/service/VisitService.java
@@ -1,15 +1,13 @@
+
 package org.springframework.samples.petclinic.service;
 
-import java.util.Collection;
-import java.util.Optional; 
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.MedicalRecord;
-import org.springframework.samples.petclinic.model.Prescription;
 import org.springframework.samples.petclinic.model.Visit;
 import org.springframework.samples.petclinic.repository.MedicalRecordRepository;
-import org.springframework.samples.petclinic.repository.PrescriptionRepository;
 import org.springframework.samples.petclinic.repository.VisitRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,35 +16,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class VisitService {
 
 	@Autowired
-	private VisitRepository visitRepository;
-	
+	private VisitRepository			visitRepository;
+
 	@Autowired
-	private MedicalRecordRepository medicalRepository;
-	
-	@Autowired
-	private PrescriptionRepository prescriptionRepository;
-	
+	private MedicalRecordRepository	medicalRepository;
+
+
 	@Transactional(readOnly = true)
-	public Optional<Visit> findVisitById(int visitId) throws DataAccessException{
-		return visitRepository.findById(visitId);
+	public Optional<Visit> findVisitById(final int visitId) throws DataAccessException {
+		return this.visitRepository.findById(visitId);
 	}
-	
-//	@Transactional
-//	public void saveVisit(Visit visit) throws DataAccessException{
-//		this.visitRepository.save(visit);
-//	}
-	
+
 	@Transactional
-	public void delete(Visit visit) throws DataAccessException{
-		//Check the associated medical record the visit may have
+	public void delete(final Visit visit) throws DataAccessException {
+
 		MedicalRecord mr = this.medicalRepository.findOneByVisitId(visit.getId());
-		if(mr != null) {
+		if (mr != null) {
 			this.visitRepository.delete(visit);
 			this.medicalRepository.delete(mr);
 		} else {
 			this.visitRepository.delete(visit);
 		}
-		
+
 	}
-	
+
 }
